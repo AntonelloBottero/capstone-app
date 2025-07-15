17064152d8629bd30049275bf69ce1f815c774b1
AntonelloBottero <antonello.bottero@gmail,com>
2025-07-15 20:35:58 +0200

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './Header/index.js';
 import Main from './Main.js';
 import Footer from './Footer/index.js';
@@ -6,11 +7,11 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/index.js';
+import Reservations from './Pages/Reservations/index.js';
 
 export default function Main() {
     return (
         <main>
-            <Home />
+            <Routes> 
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/reservations" element={<Reservations/>}></Route>
+            </Routes>
         </main>
     )
 }
